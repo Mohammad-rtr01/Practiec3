0a47d8620a6cfda111a6ab57ae0cfe2a765988a3
mohammad rtr <assd13@gmail.com>
2024-06-10 04:42:27 -0700

    compleat project

---
+++ js/app.js
@@ -0,0 +1,50 @@
+
+const A = {
+name: "Seyyed mohammad hossein" ,
+lastName: "Hashemi" ,
+age: "15" ,
+grid: "8th" ,
+
+}
+
+for(let key in A){
+
+    console.log("Hi my name is " + A.name + " and my lastname is " + A.lastName + " I'm " + A.age + " my grid is " + A.grid )
+}
+
+
+
+const B = {
+    name: "Amir hossein" ,
+    lastName: "Ahmadi" ,
+    age: "16" ,
+    grid: "9th" ,
+    
+    }
+
+    console.log("This is my classmate " + B.name + " His lastname is " + B.lastName + " He's " + B.age + " His grid is " + B.grid)
+
+
+
+    const C = {
+        name: "Mohammad mahdi" ,
+        lastName: "Ahmadi" ,
+        age: "14" ,
+        grid: "7th" ,
+        
+        }
+
+        console.log(" And this is my classmate too " + C.name + " His lastname is " + C.lastName + " He's " + C.age + " His grid is " + C.grid)
+    
+
+
+
+        const D = {
+            name: "Mohammad amin" ,
+            lastName: "Talesh amadi" ,
+            age: "16" ,
+            grid: "9th" ,
+            
+            }
+
+            console.log(" this is my third classmate " + D.name + " His lastname is " + D.lastName + " He's " + D.age + " His grid is " + D.grid)
